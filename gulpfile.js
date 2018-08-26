6cd9e09edf6f3bc43f987c5a2cc7dfc84cad29a4
Anton Varvarych <rejust.web@gmail.com>
2018-08-27 02:16:47 +0300

    All blocks whithout content

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,9 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js', // Берем Magnific Popup
+        'app/libs/bootstrap3/js/bootstrap.js',
+        'app/libs/bootstrap3/js/npm.js'
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
